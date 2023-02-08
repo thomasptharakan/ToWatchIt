9f252789bd8dc8f6caf60c6fbebcc1af1d01accb
Waseem Mohammad <wmohammad83@icloud.com>
2023-02-08 19:32:09 +0000

    Comments added to code

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+// =================================================================
+// MOVIE OBJECT
+// =================================================================
 var searchMovie = {
   movieTitle: "",
   movieYear: "",
@@ -9,11 +12,14 @@ var searchMovie = {
   youtubeTrailerURL: "",
 };
 
+// =================================================================
+// SEARCH MOVIE FUNCTION
+// =================================================================
 $("#searchButton").on("click", function (event) {
   event.preventDefault();
   var movieName = $("#searchMovie").val();
   getMovieDetails(movieName);
-  $("#searchMovie").val("")
+  $("#searchMovie").val("");
 });
 
 function getMovieDetails(movieName) {
@@ -22,6 +28,9 @@ function getMovieDetails(movieName) {
   console.log(searchMovie);
 }
 
+// =================================================================
+// QUERY OMDB API
+// =================================================================
 function getOMDBApi(movieName) {
   //Query the OMDB API
   var queryURL =
@@ -44,16 +53,14 @@ function getOMDBApi(movieName) {
     searchMovie.movieYear = response.Year;
     searchMovie.moviePoster = response.Poster;
     //Add Search Result to Screen
-    getYouTubeAPI(response.Title)
-
-
+    getYouTubeAPI(response.Title);
   });
 }
 
+// =================================================================
+// QUERY YOUTUBE API
+// =================================================================
 function getYouTubeAPI(movieName) {
-  // // =================================================================
-  // // YOUTUBE API
-  // // =================================================================
   var search = movieName + " trailer";
   var queryURL =
     "https://www.googleapis.com/youtube/v3/search?key=" +
@@ -77,10 +84,11 @@ function getYouTubeAPI(movieName) {
   });
 }
 
-
 function addSearchResult() {
   $("#result").empty();
-  // MAKE A CARD TO SHOW RESULTS
+  // =================================================================
+  // MAKE A NEW CARD TO SHOW RESULTS
+  // =================================================================
   var newCard = $("<div>");
   newCard.addClass("card mb-3");
 
@@ -92,34 +100,35 @@ function addSearchResult() {
 
   var title = $("<h5>");
   title.addClass("card-title");
-  title.attr('id', 'searchMovieTitle');
+  title.attr("id", "searchMovieTitle");
   title.text(searchMovie.movieTitle);
   cardBody.append(title);
 
   var cardPlot = $("<p>");
   cardPlot.addClass("card-text");
-  cardPlot.attr('id', 'seacrhMoviePlot');
+  cardPlot.attr("id", "seacrhMoviePlot");
   cardPlot.text(searchMovie.moviePlot);
   cardBody.append(cardPlot);
 
   var cardRating = $("<p>");
-  cardRating.attr('id', 'seacrhMovieRating');
-  cardRating.text(`Rating: ${searchMovie.movieRating} (${searchMovie.movieRatingSource})`);
+  cardRating.attr("id", "seacrhMovieRating");
+  cardRating.text(
+    `Rating: ${searchMovie.movieRating} (${searchMovie.movieRatingSource})`
+  );
   cardBody.append(cardRating);
 
   var cardActors = $("<p>");
   cardActors.addClass("card-text");
-  cardActors.attr('id', 'searchMovieActors');
+  cardActors.attr("id", "searchMovieActors");
   cardActors.text(searchMovie.movieActors);
   cardBody.append(cardActors);
 
   var colButtons = $("<div>");
   colButtons.addClass("col-md-2 d-grid gap-2 p-2");
 
-
   var addBtn = $("<button>");
   addBtn.addClass("btn btn-success");
-  addBtn.attr('id', "addMovie");
+  addBtn.attr("id", "addMovie");
   var btnIcon = $("<i>");
   btnIcon.addClass("bi bi-plus-square");
   addBtn.text(" Add To List");
@@ -128,34 +137,28 @@ function addSearchResult() {
 
   var trailerBtn = $("<button>");
   trailerBtn.addClass("btn btn-dark");
-  trailerBtn.attr('id', "searchMovieTrailer");
-  trailerBtn.attr('data-url', searchMovie.youtubeTrailerURL);
-  trailerBtn.attr('data-bs-toggle', 'modal');
-  trailerBtn.attr('data-bs-target', '#exampleModal');
+  trailerBtn.attr("id", "searchMovieTrailer");
+  trailerBtn.attr("data-url", searchMovie.youtubeTrailerURL);
+  trailerBtn.attr("data-bs-toggle", "modal");
+  trailerBtn.attr("data-bs-target", "#exampleModal");
   var trlBtnIcon = $("<i>");
   trlBtnIcon.addClass("bi bi-film");
   trailerBtn.text(" Trailer");
   trailerBtn.prepend(trlBtnIcon);
   colButtons.append(trailerBtn);
 
-
-
-
-
   var colPoster = $("<div>");
   colPoster.addClass("col-md-2");
 
   var poster = $("<img>");
   poster.addClass("img-fluid rounded-start");
-  poster.attr('id', 'searchMoviePoster');
+  poster.attr("id", "searchMoviePoster");
   poster.attr("src", searchMovie.moviePoster);
   colPoster.append(poster);
-
 
   var colDiv8 = $("<div>");
   colDiv8.addClass("col-md-8");
   colDiv8.append(cardBody);
-
 
   rowDiv.append(colPoster);
   rowDiv.append(colDiv8);
@@ -163,28 +166,29 @@ function addSearchResult() {
   newCard.append(rowDiv);
 
   $("#result").append(newCard);
-  $('#resultsContainer').attr('style', 'display:block');
-
-
+  $("#resultsContainer").attr("style", "display:block");
 }
 
-
-//Add Event listener to capture click on Search div 
-$('#result').on('click', () => addtosearchList(event));
+// =================================================================
+// ADD EVENT LISTENER TO CAPTURE THE CLICK ON THE SEARCH DIV
+// =================================================================
+$("#result").on("click", () => addtosearchList(event));
 
 function addtosearchList(event) {
   var clickedButton = event.target;
-  console.log($(clickedButton).attr('id'));
-  if ($(clickedButton).attr('id') === 'searchMovieTrailer') {
-    var searchMovieTrailerURL = $('#searchMovieTrailer').attr('data-url');
+  console.log($(clickedButton).attr("id"));
+  if ($(clickedButton).attr("id") === "searchMovieTrailer") {
+    var searchMovieTrailerURL = $("#searchMovieTrailer").attr("data-url");
     console.log(searchMovieTrailerURL);
-    $('#exampleModalLabel').text(searchMovie.movieTitle);
-    $('.modal-body iframe').attr('src', searchMovieTrailerURL);
-  } else if (($(clickedButton).attr('id') === 'addMovie')) {
-    $('#result').empty();
-    $('#resultsContainer').attr('style','display:none');
-    // Add a local Storage Entry
-    var movieDB = localStorage.getItem('movieDB');
+    $("#exampleModalLabel").text(searchMovie.movieTitle);
+    $(".modal-body iframe").attr("src", searchMovieTrailerURL);
+  } else if ($(clickedButton).attr("id") === "addMovie") {
+    $("#result").empty();
+    $("#resultsContainer").attr("style", "display:none");
+    // =================================================================
+    // ADD TO LOCAL STORAGE
+    // =================================================================
+    var movieDB = localStorage.getItem("movieDB");
     if (movieDB === null) {
       movieDB = [];
       movieDB[0] = searchMovie;
@@ -192,82 +196,75 @@ function addtosearchList(event) {
       movieDB = JSON.parse(movieDB);
       movieDB.unshift(searchMovie);
     }
-    localStorage.setItem('movieDB', JSON.stringify(movieDB));
+    localStorage.setItem("movieDB", JSON.stringify(movieDB));
 
     const toastLiveExample = document.getElementById("liveToast");
+    // =================================================================
+    // TRIGGER TOAST WITH MOVIE NAME
+    // =================================================================
     const toast = new bootstrap.Toast(toastLiveExample);
-    $('#toast-title').text(searchMovie.movieTitle);
-    $('#toast-text').text('Movie Added to your Watch-List');
+    $("#toast-title").text(searchMovie.movieTitle);
+    $("#toast-text").text("Movie Added to your Watch-List");
     toast.show();
   }
-  
-  populateSearchResults();
 
+  populateSearchResults();
 }
 
-
-//Add Event listener to capture click on saved Movies 
-$('#SavedMovies').on('click', () => removeFromList(event));
+// =================================================================
+// EVENT LISTENER TO CAPTURE CLICK ON SAVED MOVIES TO REMOVE
+// FROM LOCAL STORAGE
+// =================================================================
+$("#SavedMovies").on("click", () => removeFromList(event));
 
 function removeFromList(event) {
   var clickedButton = event.target;
-  if ($(clickedButton).attr('id') === 'watchTrailer') {
-    var searchMovieTrailerURL = $(clickedButton).attr('data-url');
-    var movieTitle = $(clickedButton).attr('data-title');
-    $('#exampleModalLabel').text(movieTitle);
-    $('.modal-body iframe').attr('src', searchMovieTrailerURL);
-  } else if (($(clickedButton).attr('id') === 'DeleteMovie')) {
+  if ($(clickedButton).attr("id") === "watchTrailer") {
+    var searchMovieTrailerURL = $(clickedButton).attr("data-url");
+    var movieTitle = $(clickedButton).attr("data-title");
+    $("#exampleModalLabel").text(movieTitle);
+    $(".modal-body iframe").attr("src", searchMovieTrailerURL);
+  } else if ($(clickedButton).attr("id") === "DeleteMovie") {
     // Add a local Storage Entry
-    var deleteItem = $(clickedButton).attr('data-id');
-    var movieDB = localStorage.getItem('movieDB');
-    var movieTitle = 'MovieName';
+    var deleteItem = $(clickedButton).attr("data-id");
+    var movieDB = localStorage.getItem("movieDB");
+    var movieTitle = "MovieName";
     if (!(movieDB === null)) {
       movieDB = JSON.parse(movieDB);
       movieTitle = movieDB[deleteItem].movieTitle;
-      movieDB.splice(deleteItem,1);
+      movieDB.splice(deleteItem, 1);
     }
-    localStorage.setItem('movieDB', JSON.stringify(movieDB));
+    localStorage.setItem("movieDB", JSON.stringify(movieDB));
 
     const toastLiveExample = document.getElementById("liveToast");
     const toast = new bootstrap.Toast(toastLiveExample);
-    $('#toast-title').text(movieTitle);
-    $('#toast-text').text('Movie Removed from your List');
+    $("#toast-title").text(movieTitle);
+    $("#toast-text").text("Movie Removed from your List");
     toast.show();
   }
   populateSearchResults();
-
 }
 
-// // =================================================================
-// // TOAST
-// // =================================================================
-
-// const toastTrigger = document.getElementById("DeleteMovie");
-
-// if (toastTrigger) {
-//   toastTrigger.addEventListener("click", (event) => {
-//     const toast = new bootstrap.Toast(toastLiveExample);
-//     var addMovieTitle = $('#result').eq('<h5>').text;
-//     alert(addMovieTitle);
-//     toast.show();
-//   });
-// }
-
-
+// =================================================================
+// CREATE CARDS FROM SAVED LOCAL STORAGE
+// =================================================================
 function populateSearchResults() {
-
-
-  //Added to get keys from localStorage if key.js is not included.
-  if ((omdbApiKey==null)||(youTubeApiKey==null)){
-    youTubeApiKey = localStorage.getItem('youTubeApiKey');
-    omdbApiKey = localStorage.getItem('omdbApiKey');
+  // =================================================================
+  // ADDED TO GET THE KEYS FROM LOCAL STORAGE IF KEYS.JS FILE
+  // IS NOT FOUND
+  // =================================================================
+  if (omdbApiKey == null || youTubeApiKey == null) {
+    youTubeApiKey = localStorage.getItem("youTubeApiKey");
+    omdbApiKey = localStorage.getItem("omdbApiKey");
   }
-  var movieDB = localStorage.getItem('movieDB');
+  var movieDB = localStorage.getItem("movieDB");
   movieDB = JSON.parse(movieDB);
   if (!(movieDB === null)) {
     $("#movieDB").empty();
+    // =================================================================
+    // NEW CARD FOR EACH SAVED MOVIE
+    // =================================================================
     for (i in movieDB) {
-      // MAKE A CARD TO SHOW SAVED MOVIES
       var newCard = $("<div>");
       newCard.addClass("card mb-3");
 
@@ -288,7 +285,9 @@ function populateSearchResults() {
       cardBody.append(cardPlot);
 
       var cardRating = $("<p>");
-      cardRating.text(`Rating: ${movieDB[i].movieRating} (${movieDB[i].movieRatingSource})`);
+      cardRating.text(
+        `Rating: ${movieDB[i].movieRating} (${movieDB[i].movieRatingSource})`
+      );
       cardBody.append(cardRating);
 
       var cardActors = $("<p>");
@@ -299,11 +298,10 @@ function populateSearchResults() {
       var colButtons = $("<div>");
       colButtons.addClass("col-md-2 d-grid gap-2 p-2");
 
-
       var addBtn = $("<button>");
       addBtn.addClass("btn btn-success");
-      addBtn.attr('id', "DeleteMovie");
-      addBtn.attr('data-id',i);
+      addBtn.attr("id", "DeleteMovie");
+      addBtn.attr("data-id", i);
       var btnIcon = $("<i>");
       btnIcon.addClass("bi bi-check2-circle");
       addBtn.text(" Watched It");
@@ -312,11 +310,11 @@ function populateSearchResults() {
 
       var trailerBtn = $("<button>");
       trailerBtn.addClass("btn btn-dark");
-      trailerBtn.attr('id', "watchTrailer");
-      trailerBtn.attr('data-url',movieDB[i].youtubeTrailerURL);
-      trailerBtn.attr('data-title',movieDB[i].movieTitle);
-      trailerBtn.attr('data-bs-toggle', 'modal');
-      trailerBtn.attr('data-bs-target', '#exampleModal');
+      trailerBtn.attr("id", "watchTrailer");
+      trailerBtn.attr("data-url", movieDB[i].youtubeTrailerURL);
+      trailerBtn.attr("data-title", movieDB[i].movieTitle);
+      trailerBtn.attr("data-bs-toggle", "modal");
+      trailerBtn.attr("data-bs-target", "#exampleModal");
       var trlBtnIcon = $("<i>");
       trlBtnIcon.addClass("bi bi-film");
       trailerBtn.text(" Trailer");
@@ -331,11 +329,9 @@ function populateSearchResults() {
       poster.attr("src", movieDB[i].moviePoster);
       colPoster.append(poster);
 
-
       var colDiv8 = $("<div>");
       colDiv8.addClass("col-md-8");
       colDiv8.append(cardBody);
-
 
       rowDiv.append(colPoster);
       rowDiv.append(colDiv8);
@@ -343,9 +339,8 @@ function populateSearchResults() {
       newCard.append(rowDiv);
 
       $("#movieDB").append(newCard);
-      $('#SavedMovies').attr('style', 'display:block');
+      $("#SavedMovies").attr("style", "display:block");
     }
-
   }
 }
 
@@ -354,6 +349,9 @@ function populateSearchResults() {
 // =================================================================
 $(".btn-close").on("click", function () {
   $("#modalTrailer").attr("src", "");
- });
+});
 
+// =================================================================
+// CALLING THE FUNCTION TO POPULATE SAVED MOVIES
+// =================================================================
 populateSearchResults();
