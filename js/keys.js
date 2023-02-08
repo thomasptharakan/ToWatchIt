2d139720c71b4ebc675a082929f273e1eb9dc624
Thomas Tharakan <thomasptharakan@gmail.com>
2023-02-08 18:21:42 +0000

    Add fix from Waseem for Icon

--- js/keys.js
+++ js/keys.js
@@ -1,2 +1,5 @@
 var youTubeApiKey  = null;
 var omdbApiKey = null;
+// var youTubeApiKey  = "AIzaSyAhNnIG1SVs_HPDf1IeG4Wn6Lv9nhxkGck";
+// var youTubeApiKey  = "AIzaSyBfNmM9dcR_tbBu_JP6HT-6Q6NaFo00bKM";
+// var omdbApiKey = "trilogy";
